3737ccae03ad657390c1b8a5424b9c44ddd6e26c
parkyc <erty1201@naver.com>
2026-08-24 00:44:57 +0900

    Mono-Repo 세팅, 초기화면 생성

---
+++ api/src/main/java/com/parkyc/jypword/JypWordApplication.java
@@ -0,0 +1,13 @@
+package com.parkyc.jypword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JypWordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JypWordApplication.class, args);
+    }
+
+}
